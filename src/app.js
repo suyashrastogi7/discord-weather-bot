69fba1bf0652c4ab88d890558412663d638cff75
suyashrastogi7 <suyashrastogi@protonmail.com>
2022-01-26 17:36:41 +0530

    Added Error Functions

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const { Client } = require("discord.js");
-// const CheckInput = require("./_helpers/check-string");
-// const getLocation = require("./_helpers/get-location");
+
 const data = require("./_service/data");
 
 //New Client Instance
@@ -26,23 +25,32 @@ client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
   const MESSAGE_LC = message.content.trim().toLowerCase();
   var input = MESSAGE_LC.split(" ");
+  if (MESSAGE_LC === "!weather") {
+    message.reply("Use !weather <city_name> to Begin!");
+    return;
+  }
   if (input[0] === "!weather") {
-    var location = input[1];
-    var weather = await data(location);
-    var umbrella = "";
-    if (weather.clouds.all > 65) {
-      umbrella = "You Better Take an Umbrella!";
+    if (input[1] === "help") {
+      message.reply("Use !weather <city_name> to Begin!");
+      return;
     } else {
-      umbrella = "Doesn't look like You need an Umbrella";
-    }
-    message.reply(`
+      var location = input[1];
+      var weather = await data(location);
+      var umbrella = "";
+      if (weather.clouds.all > 65) {
+        umbrella = "You Better Take an Umbrella!";
+      } else {
+        umbrella = "Doesn't look like You need an Umbrella";
+      }
+      message.reply(`
       Your City : ${weather.name} has ${weather.weather[0].main} weather today.
       Maximum Temperature is ${weather.main.temp_max} Celcius and Minimum Temperature is ${weather.main.temp_min} Celcius
       With Pressure of ${weather.main.pressure} Bar and Humidity of ${weather.main.humidity}
       Wind Speed is ${weather.wind.speed}
       Cloud Cover is ${weather.clouds.all}% so ${umbrella}
     `);
-    return;
+      return;
+    }
   } else {
     message.reply("Incorrect Syntax");
     return;
